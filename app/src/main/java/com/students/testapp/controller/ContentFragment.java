7e8e685e6fcc95524bec95a27ec1881de8abe520
itandrik <itcherry97@gmail.com>
2017-02-26 15:11:45 +0200

    NavigationDrawer separated from content

---
+++ app/src/main/java/com/students/testapp/controller/ContentFragment.java
@@ -0,0 +1,150 @@
+package com.students.testapp.controller;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.students.testapp.R;
+import com.students.testapp.controller.adapter.StudentsAdapter;
+import com.students.testapp.controller.exception.ControllerException;
+import com.students.testapp.controller.listener.PaginationScrollListener;
+import com.students.testapp.model.db.StudentDatabase;
+import com.students.testapp.model.entity.Student;
+import com.students.testapp.model.manager.ApiManager;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+/**
+ * @author Andrii Chernysh.
+ *         E-mail : itcherry97@gmail.com
+ */
+public class ContentFragment extends Fragment {
+    private RecyclerView mRecyclerView;
+    private LinearLayoutManager mLayoutManager;
+    private LinearLayout mEmptyList;
+    private StudentsAdapter mAdapter;
+    private StudentDatabase mDatabase;
+    public static final int THRESHOLD = 20;
+    private long rowsOffset = THRESHOLD;
+    private long totalStudentsCount;
+    private boolean isLoading = false;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.content_main, container);
+        mDatabase = new StudentDatabase(getActivity());
+
+        initRecyclerView(v);
+        processRecyclerScrolling();
+        fetchStudentsFromServer();
+
+        return v;
+    }
+
+    private void initRecyclerView(View v) {
+        mEmptyList = (LinearLayout) v.findViewById(R.id.empty_list);
+        mAdapter = new StudentsAdapter(getActivity());
+        mRecyclerView = (RecyclerView) v.findViewById(R.id.students_container_recycler);
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    private void processRecyclerScrolling() {
+        mRecyclerView.addOnScrollListener(new PaginationScrollListener(mLayoutManager) {
+            @Override
+            protected void loadMoreItems() {
+                isLoading = true;
+
+                new Handler().post(new Runnable() {
+                    @Override
+                    public void run() {
+                        List<Student> newStudents =
+                                mDatabase.getCertainNumberOfStudents(THRESHOLD, rowsOffset, null);
+                        mAdapter.addStudents(newStudents);
+                    }
+                });
+
+                isLoading = false;
+                rowsOffset += THRESHOLD;
+            }
+
+            @Override
+            public long getTotalStudentCount() {
+                return totalStudentsCount;
+            }
+
+            @Override
+            public boolean isLoading() {
+                return isLoading;
+            }
+        });
+    }
+
+    private void fetchStudentsFromServer() {
+        Call<List<Student>> call = ApiManager.getInstance().fetchStudents();
+        call.enqueue(new Callback<List<Student>>() {
+            @Override
+            public void onResponse(Call<List<Student>> call, Response<List<Student>> response) {
+                List<Student> studentsList = response.body();
+                mAdapter.addStudents(studentsList.subList(0, THRESHOLD));
+                new StudentsInsertDbTask().execute(studentsList);
+            }
+
+            @Override
+            public void onFailure(Call<List<Student>> call, Throwable t) {
+                Log.e("LOG_TAG", t.getMessage());
+                throw new ControllerException()
+                        .addMessage("Problems with fetching data from server")
+                        .addLogMessage("Problems with fetching data from server" + t.getLocalizedMessage())
+                        .setClassThrowsException(MainActivity.class);
+            }
+        });
+    }
+
+    private class StudentsInsertDbTask extends AsyncTask<List<Student>, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mRecyclerView.setVisibility(View.GONE);
+            mEmptyList.setVisibility(View.VISIBLE);
+            isLoading = true;
+        }
+
+        @SafeVarargs
+        @Override
+        protected final Void doInBackground(List<Student>... studentsList) {
+            for (List<Student> param : studentsList) {
+                mDatabase.bulkInsertStudents(param);
+                totalStudentsCount = param.size();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            mRecyclerView.setVisibility(View.VISIBLE);
+            mEmptyList.setVisibility(View.GONE);
+            isLoading = false;
+            mDatabase.selectCountOfStudents();
+        }
+    }
+}
--- app/src/main/java/com/students/testapp/controller/MainActivity.java
+++ app/src/main/java/com/students/testapp/controller/MainActivity.java
@@ -1,53 +1,29 @@
 package com.students.testapp.controller;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 
 import com.students.testapp.R;
-import com.students.testapp.controller.adapter.StudentsAdapter;
-import com.students.testapp.controller.exception.ControllerException;
-import com.students.testapp.controller.listener.PaginationScrollListener;
 import com.students.testapp.model.db.DatabaseContract;
 import com.students.testapp.model.db.StudentDatabase;
-import com.students.testapp.model.entity.Student;
-import com.students.testapp.model.manager.ApiManager;
-
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 /**
  * @author Andrii Chernysh.
  *         E-mail : itcherry97@gmail.com
  */
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener{
     private DrawerLayout mDrawer;
-    private RecyclerView mRecyclerView;
-    private LinearLayout mEmptyList;
-    private StudentsAdapter mAdapter;
     private StudentDatabase mDatabase;
-    public static final int THRESHOLD = 20;
-    private long rowsOffset = THRESHOLD;
-    private long totalStudentsCount;
-    private boolean isLoading = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,65 +45,6 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        /* Recycler view initialization*/
-        mEmptyList = (LinearLayout) findViewById(R.id.empty_list);
-        mAdapter = new StudentsAdapter(this);
-        mRecyclerView = (RecyclerView) findViewById(R.id.students_container_recycler);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(layoutManager);
-        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
-        mRecyclerView.setAdapter(mAdapter);
-        mRecyclerView.addOnScrollListener(new PaginationScrollListener(
-                (LinearLayoutManager) layoutManager) {
-            @Override
-            protected void loadMoreItems() {
-                isLoading = true;
-
-                new Handler().post(new Runnable() {
-                    @Override
-                    public void run() {
-                        List<Student> newStudents =
-                                mDatabase.getCertainNumberOfStudents(THRESHOLD, rowsOffset, null);
-                        mAdapter.addStudents(newStudents);
-                    }
-                });
-
-                isLoading = false;
-                rowsOffset += THRESHOLD;
-            }
-
-            @Override
-            public long getTotalStudentCount() {
-                return totalStudentsCount;
-            }
-
-            @Override
-            public boolean isLoading() {
-                return isLoading;
-            }
-        });
-        fetchStudentsFromServer();
-    }
-
-    private void fetchStudentsFromServer() {
-        Call<List<Student>> call = ApiManager.getInstance().fetchStudents();
-        call.enqueue(new Callback<List<Student>>() {
-            @Override
-            public void onResponse(Call<List<Student>> call, Response<List<Student>> response) {
-                List<Student> studentsList = response.body();
-                mAdapter.addStudents(studentsList.subList(0, THRESHOLD));
-                new FetchStudentsTask().execute(studentsList);
-            }
-
-            @Override
-            public void onFailure(Call<List<Student>> call, Throwable t) {
-                Log.e("LOG_TAG", t.getMessage());
-                throw new ControllerException()
-                        .addMessage("Problems with fetching data from server")
-                        .addLogMessage("Problems with fetching data from server" + t.getLocalizedMessage())
-                        .setClassThrowsException(MainActivity.class);
-            }
-        });
     }
 
     private Toolbar getConfiguredToolbar() {
@@ -159,9 +76,9 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
-        int id = item.getItemId();
+        /*int id = item.getItemId();
 
-        /*if (id == R.id.nav_camera) {
+        if (id == R.id.nav_camera) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
@@ -187,34 +104,5 @@ public class MainActivity extends AppCompatActivity
         mDatabase.clearTable(DatabaseContract.COURSE_TABLE_NAME);
         mDatabase.clearTable(DatabaseContract.STUDENT_HAS_COURSE_TABLE_NAME);
         mDatabase.closeDatabaseConnection();
-    }
-
-    private class FetchStudentsTask extends AsyncTask<List<Student>, Void, Void> {
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            mRecyclerView.setVisibility(View.GONE);
-            mEmptyList.setVisibility(View.VISIBLE);
-            isLoading = true;
-        }
-
-        @SafeVarargs
-        @Override
-        protected final Void doInBackground(List<Student>... studentsList) {
-            for (List<Student> param : studentsList) {
-                mDatabase.bulkInsertStudents(param);
-                totalStudentsCount = param.size();
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-            mRecyclerView.setVisibility(View.VISIBLE);
-            mEmptyList.setVisibility(View.GONE);
-            isLoading = false;
-            mDatabase.selectCountOfStudents();
-        }
     }
 }
